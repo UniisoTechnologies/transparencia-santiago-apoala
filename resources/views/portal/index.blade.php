d62dfd0ac301101b4123cdd37dbe1fa451af99f6
MarcossDiazPerez <diazperezmarcoss@gmail.com>
2019-12-15 06:47:18 -0600

    index

--- resources/views/portal/index.blade.php
+++ resources/views/portal/index.blade.php
@@ -1,97 +1,115 @@
 
 @extends('plantilla')
 @section('seccion')
-<div class="container">
- 
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
-                  </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-        </div>
+ <!--carrusel  -->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="images/cascadas/IMG_3382.JPG">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="images/cascadas/CAM03291.jpg">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="images/cascadas/CAM02978.jpg">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+</div>
 <!--/.Carousel Wrapper-->
-</div>
-<center>
-
-<div class="col-md-10 alert alert-secondary" role="alert" style="margin-top: 2%;">
-  <center>
-        <h3>
-        Santiago Apoala, Oax.
-        </h3>
-  </center>
-</div>
-</center>
- <center>
- <div class="blog-post col-md-9">
-            <p>Santiago Apoala población que pertenece al municipio Nochixtlán en el estado de Oaxaca (región Mixteca)</p>
-            <hr>
-            <p>Santiago Apoala se localiza a 125 kilómetros al poniente de la ciudad de Oaxaca.</p>
-            <p>Apoala ofrece al visitante bellezas naturales sin igual: peñas, cañones, cascadas y valles, cuevas con amplias galerías y miradores naturales.</p>
-            <div class="container">
-  <div class="row">
-    <div class="col-sm">
-     <center><img src="images/cascadas/cascada.jpg" alt="Responsive image" class="img-thumbnail" style="width: 400px"></center>
+<div class="container">
+<div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class = "row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 alert alert-secondary" role="alert" style="margin-top: 2%;">
+                    <h3 class="text-center">Santiago Apoala, Oax.</h3>
+            </div>
+            <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                <p>Población que pertenece al municipio de Nochixtlán en el estado de Oaxaca (región Mixteca)</p>
+                <hr>
+                <p>Se localiza a 125 kilómetros al poniente de la ciudad de Oaxaca.</p>
+                <p>Ofrece al visitante bellezas naturales sin igual: peñas, cañones, cascadas y valles, cuevas con amplias galerías y miradores naturales.</p>
+            </div>
+            <div class="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                <blockquote><p style="margin-top: 1%;"><strong>Apoala</strong> que significa: donde reparten el agua en mixteco.</p></blockquote>
+                <p>Como complemento a sus bellezas naturales, las manifestaciones de la cultura de Apoala hallan uno de sus mejores
+                    ejemplos en el templo de Santiago Apóstol, probablemente erigido en el siglo XVI. De pequeñas dimensiones, 
+                    su fachada principal está compuesta por una portada y una torre con un campanario de un solo cuerpo. 
+                    En las esquinas del atrio existen capillas posas, comunes en la época de su construcción.</p>
+            </div>
+        </div> 	
     </div>
-    <div class="col-sm">
-     <center><img src="images/cascadas/santiago2.jpg" alt="Responsive image" class="img-thumbnail" style="width: 400px"></center>
-    </div>
-    
-  </div>
 </div>
-            
-            <blockquote>
-              <p style="margin-top: 1%;"><strong>Apoala</strong> que significa: donde reparten el agua en mixteco.</p>
-            </blockquote>
-            <p>Como complemento a sus bellezas naturales, las manifestaciones de la cultura de Apoala hallan uno de sus mejores ejemplos en el templo de Santiago Apóstol, probablemente erigido en el siglo XVI. De pequeñas dimensiones, su fachada principal está compuesta por una portada y una torre con un campanario de un solo cuerpo. En las esquinas del atrio existen capillas posas, comunes en la época de su construcción.
-
-
-              <p><strong>Santiago Apoala se localiza a 125 kilómetros al poniente de la ciudad de Oaxaca.</strong></p>
-            
-          </div><!-- /.blog-post -->
+<div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div class = "row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 alert alert-secondary" role="alert" style="margin-top: 2%;">
+                        <h3 class="text-center">Sección de noticias</h3>
+                        <h4 class="text-center">Entérate de las noticias de nuestro municipio</h4>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                    <div class="card">
+                         <img class="card-img-top" src="images/noticias/cabildo.jpeg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                    <div class="card">
+                            <img class="card-img-top" src="images/noticias/foto-transformador-1024x683.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Renueva el transformador que operaba en Camino santa Inés pozo #2</h5>
+                            <p class="card-text">El Honorable Ayuntamiento en coordinación con la Regiduria de Agua Potable 
+                                                 Renueva el transformador que operaba en
+                                                 Camino santa Inés pozo #2 por uno más eficaz,ahora de 112.5 Kva…..</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                    <div class="card">
+                            <img class="card-img-top" src="images/cabildo.jpeg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div> 	
         </div>
-</center>
+</div>
+
+
+
+
+
+
+</div>
+
 
 <div class="container mt-4"> <!--/.div contenedor de seccion-->
 
 <section>
        <!--Grid fila-->
          <div class="row">
-                   <!--Grid columna-->
-                    <div class="col-md-6 mb-4">
-                        <!--Card-->
-                        <div class="card">
-                            <!--Card contenido-->
-                            <div class="card-body text-center">
-                                <h4 class="font-up font-bold deep-purple-text mb-3"><strong>Ubicanos:</strong></h4>
-                                <!--Google mapa-->
-                                <div id="map-responsive" class="z-depth-1" style="height: 400px">
-                                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15208.163228465033!2d-97.1445807796377!3d17.648226204662823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c663cce175f115%3A0x5c4169323d49a9f8!2sSantiago+Apoala%2C+Oax.!5e0!3m2!1ses-419!2smx!4v1539052680100" width="400" height="350" frameborder="0" style="border:0; padding-top: 1em;" allowfullscreen></iframe>
-                                </div>
-                            </div>
-                           <!--/.Card contenido-->
-                        </div>
-                        <!--/.Card-->
-                    </div>
+
 
                     <div class="col-md-6 mb-4">
                         <!--Card-->
